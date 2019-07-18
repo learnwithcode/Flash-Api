cf81df0dd74d6344f76d9ac6bf1871200176fecf
Abhiraj Pundir <pundirabhiraj.com>
2019-07-18 16:35:30 +0530

    Updates

---
+++ precache-manifest.0cd5cb7c3427a97adb24e8aa86550706.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4fdc96933b581499d43e21d0de48a5a",
+    "url": "/Flash-Api/index.html"
+  },
+  {
+    "revision": "c85229fb10fc90863253",
+    "url": "/Flash-Api/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "652e691c10dd16569187",
+    "url": "/Flash-Api/static/js/2.8b429a8f.chunk.js"
+  },
+  {
+    "revision": "c85229fb10fc90863253",
+    "url": "/Flash-Api/static/js/main.712c260a.chunk.js"
+  },
+  {
+    "revision": "ca4129c6b8bd65ee3114",
+    "url": "/Flash-Api/static/js/runtime~main.5d0c9f23.js"
+  }
+]);
