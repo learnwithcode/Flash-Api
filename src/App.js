7a14b13227400179214f76e64792380a4a06d0e9
Abhiraj Pundir <pundirabhiraj.com>
2019-07-19 16:27:57 +0530

    flash api

--- src/App.js
+++ src/App.js
@@ -1,58 +1,66 @@
 import React from 'react';
 import axios from 'axios';
 
-
+      // const { mobile, first_name, employee_id, services } = this.state;
+      // const data = { mobile, first_name, employee_id, services };
+      // const conf = {
+      //   method: "post",
+      //   body: JSON.stringify(data),
+      //   headers: new Headers({ "Content-Type": "application/x-www-form-urlencoded","AUTHORIZATION": "Rkx7@a5FHl-L#6Fb9CzF2L-jNz7epF4mEtC"})
+      // };
+      // fetch('http://127.0.0.1:8000/extension/flash-api', conf).then(response => console.log(response));
 
 class App extends React.Component {
   state = {
       mobile: '',
       first_name : '',
       employee_id : '',
-      billing_amount : '',
       services : '',
+    
   }
-
-  onClick = () => {
+  onFormSubmit = (event) => {
+      event.preventDefault();
       const data = {
         mobile :this.state.mobile,
         first_name : this.state.first_name,
         employee_id : this.state.employee_id,
-        billing_amount : this.billing_amount,
-        services : this.state.services
+        services : this.state.services,
       }
       const token = "Rkx7@a5FHl-L#6Fb9CzF2L-jNz7epF4mEtC"
       const headers = {
-        
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Accept" : "application/json, text/plain, */*",
-        "Token": token,
+        "Content-Type": "application/x-www-form-urlencoded",
+        "AUTHORIZATION": token,
         
         
       }
-      axios.post('https://fellafeeds.com/extension/flash-api', data, {headers:headers})
+
+      axios.post('https://fellafeeds.com/extension/flash-api', data, {headers : headers})
       .then(response => {
         console.log(response)
       })
       .catch(err => {
         console.log(err)
       });
+      
   }
-
+  onChange = e => {
+    this.setState({[e.target.name]: e.target.value})
+}
   render () {
     return (
-      <div>
+      <form onSubmit={(event) => this.onFormSubmit(event)}>
         <div className="form-group">
           <label htmlFor="mobile">Mobile</label>
           <input type="text" name='mobile' className="form-control" id="mobile" aria-describedby="emailHelp" placeholder="Enter Mobile"
           value={this.state.mobile}
-          onChange={e => this.setState({mobile : e.target.value})}/>
+          onChange={this.onChange}/>
         </div>
 
         <div className="form-group">
           <label htmlFor="first_name">First Name</label>
           <input
           value={this.state.first_name} 
-          onChange={e => this.setState({first_name : e.target.value})}
+          onChange={this.onChange}
           type="text" name='first_name' className="form-control" id="first_name" placeholder="Enter First Name"/>
         </div>
 
@@ -60,28 +68,20 @@ class App extends React.Component {
           <label htmlFor="employee_id">Employee Id</label>
           <input
           value={this.state.employee_id} 
-          onChange={e => this.setState({employee_id : e.target.value})}
+          onChange={this.onChange}
           type="text" name='employee_id' className="form-control" id="employee_id" placeholder="Enter Employee Id"/>
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="billing_amount">Billing Amount</label>
-          <input
-          value={this.state.billing_amount} 
-          onChange={e => this.setState({billing_amount : e.target.value})}
-          type="text" name='billing_amount' className="form-control" id="billing_amount" placeholder="Enter Billing Amount"/>
         </div>
 
         <div className="form-group">
           <label htmlFor="services">Services</label>
           <input
           value={this.state.services} 
-          onChange={e => this.setState({services : e.target.value})}
+          onChange={this.onChange}
           type="text" name='services' className="form-control" id="services" placeholder="Enter Services"/>
         </div>
-      <button type="submit" className="btn btn-primary" onClick={this.onClick}>Submit</button>
+      <button type="submit" className="btn btn-primary">Submit</button>
 
-      </div>
+      </form>
     )
   }
 }
